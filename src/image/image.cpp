601534c955e0e2ea8c584ac2f7aab3edb1c4b354
Vladyslav Vyslobitskyi <vyslobitskyi.vlad@gmail.com>
2023-12-21 11:13:25 +0200

    reversed recognized text output order

--- src/image/image.cpp
+++ src/image/image.cpp
@@ -4,7 +4,7 @@ void image::renderContours(const cv::Mat& frame, const std::vector<cv::Rect>& co
 {
   for (size_t i = 0; i < contours.size(); i++)
   {
-    cv::rectangle(frame, contours[i], cv::Scalar(0, 255, 0), 3, 8, 0);
+    cv::rectangle(frame, contours[i], cv::Scalar(0, 255, 0), 1, 8, 0);
   }
 }
 
@@ -14,7 +14,7 @@ std::vector<cv::Rect> image::findContours(const cv::Mat& frame)
   cv::cvtColor(frame, gray, cv::COLOR_BGR2GRAY);
   cv::Sobel(gray, sobel, CV_8U, 1, 0, 3, 1, 0, cv::BORDER_DEFAULT);
   cv::threshold(sobel, threshold, 0, 255, cv::THRESH_OTSU + cv::THRESH_BINARY);
-  kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(15, 3));
+  kernel = cv::getStructuringElement(cv::MORPH_RECT, cv::Size(15, 5));
   cv::morphologyEx(threshold, threshold, cv::MORPH_CLOSE, kernel);
   std::vector<std::vector<cv::Point>> contours;
   cv::findContours(threshold, contours, 0, 1);
--- src/main.cpp
+++ src/main.cpp
@@ -14,13 +14,13 @@ int main(int argc, char** argv)
     cv::imshow("Frame", frameClone);
   };
   const cv::Mat frame = video::live(onFrame);
-  const std::vector<cv::Rect> contours = image::findContours(frame);
-  const std::string outputPath = std::filesystem::current_path().generic_string() + "found_text.txt";
+  std::vector<cv::Rect> contours = image::findContours(frame);
+  const std::string outputPath = std::filesystem::current_path().generic_string() + "/found_text.txt";
   std::filesystem::remove(outputPath);
   std::ofstream output(outputPath);
-  for (const auto& contour : contours)
+  for (std::vector<cv::Rect>::reverse_iterator i = contours.rbegin(); i != contours.rend(); ++i)
   {
-    const cv::Mat roi = frame(contour);
+    const cv::Mat roi = frame(*i);
     const std::string text = ocr::findText(roi);
     output << text << std::endl;
   }
